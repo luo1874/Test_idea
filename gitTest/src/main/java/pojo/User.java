9958af190015ff5fbf4258daa9d5549db24daf3d
Luo <luo>
2020-07-04 16:43:51 +0800

    firstCommit

---
+++ gitTest/src/main/java/pojo/User.java
@@ -0,0 +1,12 @@
+package pojo;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author luo
+ * @date 2020-07-04 16:41:09
+ */
+public class User {
+    private String name;
+    private Integer age;
+}
